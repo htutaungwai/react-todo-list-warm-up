453adbc689ea7f747ddf76867f737fa85bff2b31
Htut Aung Wai <defo19987@gmail.com>
2023-09-10 12:57:20 +0700

    finished react warm-up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import todos from "./data";
 
 function App() {
+  const [newItem, setNewItem] = useState("abcd");
+  const [items, setItems] = useState(todos);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <React.Fragment>
+      <form className="new-item-form">
+        <label>New Item</label>
+        <input
+          type="text"
+          id="item"
+          value={newItem}
+          onChange={(e) => {
+            setNewItem(e.target.value);
+          }}
+        ></input>
+        <button
+          className="btn"
+          onClick={(e) => {
+            e.preventDefault();
+            setItems([
+              ...items,
+              { title: newItem, checked: false, id: items.length + 1 },
+            ]);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Add
+        </button>
+      </form>
+
+      <h1 className="header">Todo List</h1>
+
+      <ul className="list">
+        {items.length > 0 &&
+          items.map((item) => (
+            <li id={item.id} key={item.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={item.checked}
+                  onChange={() => {
+                    const newState = items.map((obj) => {
+                      if (obj.id === item.id)
+                        return { ...obj, checked: !item.checked };
+                      return obj;
+                    });
+
+                    setItems([...newState]);
+                  }}
+                />
+                {item.title}
+              </label>
+              <button
+                className="btn btn-danger"
+                onClick={() => {
+                  const filteredState = items.filter(
+                    (obj) => obj.id !== item.id
+                  );
+
+                  setItems([...filteredState]);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+      </ul>
+    </React.Fragment>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,3 @@
+const todos = [{ title: "Item 1", id: 1, checked: true }];
+
+export default todos;
